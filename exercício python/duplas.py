c3ac5f32d9ca290acc281ed3827d06f4a7d06c27
vicenzogiovanetti <vicenzogiovanetti@gmail.com>
2026-09-19 21:41:18 +0000

    adicionando nome

--- exercício python/duplas.py
+++ exercício python/duplas.py
@@ -1,5 +1,6 @@
 #soma dos naturais de 1 a n
+nome = input("Digite seu nome:")
 n = int(input("Digite um número natural:"))
 def soma(n):
     return sum(range(1,n+1))
-print("A soma dos números naturais de 1 a",n,"é",soma(n))
+print(f"Olá, {nome}! A soma dos números naturais de 1 a {n} é {soma(n)}.")
